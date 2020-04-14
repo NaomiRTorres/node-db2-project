07346f606f6e4031fd7495ab0e55f7cd7b8553c0
Naomi Ruemmely Torres <59476335+NaomiPriest97@users.noreply.github.com>
2020-04-14 15:38:33 -0400

    saving my setup

---
+++ data/migrations/20200414141615_add_cars_table.js
@@ -0,0 +1,28 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('cars', tbl => {
+      tbl.increments();
+
+     //vin
+     tbl.string('vin', 200).notNullable().unique().index();
+
+     //make
+     tbl.string('make').notNullable();
+
+     //model
+     tbl.string('model').notNullable();
+
+     //mileage
+     tbl.integer('mileage').notNullable();
+
+     //transmission
+     tbl.string('transmission type').notNullable();
+
+     //title
+     tbl.string('title status');
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('cars');
+};
